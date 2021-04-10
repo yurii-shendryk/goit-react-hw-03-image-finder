231d56ae2135ff3c554af6edf54f433c766c78dd
yurii-shendryk <yuriishendryk93@gmail.com>
2021-04-10 23:33:14 +0300

    reset default params in  api

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -2,23 +2,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '20301479-c3cd6e26a773a7261d62d0b69';
-axios.defaults.params = {
-  key: API_KEY,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  per_page: 12,
-};
 
-const fetchImages = async ({ searchQuery = '', currentPage = 1 }) => {
-  try {
-    const { data } = await axios.get('', {
-      params: { q: searchQuery, page: currentPage },
-    });
-    return data.hits;
-  } catch (error) {
-    console.log('error', { error });
-    return [];
-  }
+const fetchImages = ({ searchQuery = '', currentPage = 1, pageSize = 12 }) => {
+  return axios
+    .get(
+      `/?q=${searchQuery}&key=${API_KEY}&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=12`,
+    )
+    .then(({ data }) => data.hits);
 };
 
 export default { fetchImages };
